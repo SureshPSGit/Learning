b4ded6a1da5b807b502a41cd802b3e1b6a33277d
Nick Chapsas <elfocrash@gmail.com>
2020-03-02 22:50:05 +0000

    Added the keyvault loading with certificate

---
+++ AzureTutorials/AzureKeyVaultExample/PrefixKeyVaultSecretManager.cs
@@ -0,0 +1,27 @@
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+
+namespace AzureKeyVaultExample
+{
+    public class PrefixKeyVaultSecretManager : IKeyVaultSecretManager
+    {
+        private readonly string _prefix;
+
+        public PrefixKeyVaultSecretManager(string prefix)
+        {
+            _prefix = $"{prefix}-";
+        }
+
+        public bool Load(SecretItem secret)
+        {
+            return secret.Identifier.Name.StartsWith(_prefix);
+        }
+
+        public string GetKey(SecretBundle secret)
+        {
+            return secret.SecretIdentifier.Name.Substring(_prefix.Length)
+                .Replace("--", ConfigurationPath.KeyDelimiter);
+        }
+    }
+}
--- AzureTutorials/AzureKeyVaultExample/Program.cs
+++ AzureTutorials/AzureKeyVaultExample/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +19,38 @@ namespace AzureKeyVaultExample
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    var root = builder.Build();
+                    var vaultName = root["KeyVault:Vault"];
+
+                    if (!string.IsNullOrEmpty(vaultName))
+                    {
+                        builder.AddAzureKeyVault($"https://{vaultName}.vault.azure.net/",
+                            root["KeyVault:ClientId"], GetCertificate(root["KeyVault:Thumbprint"]),
+                            new PrefixKeyVaultSecretManager("AzureKeyVaultExample"));
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        
+        private static X509Certificate2 GetCertificate(string thumbprint)
+        {
+            var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            try
+            {
+                store.Open(OpenFlags.ReadOnly);
+                var certificateCollection = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false);
+
+                if (certificateCollection.Count == 0)
+                {
+                    throw new Exception("Certificate is not installed");
+                }
+                return certificateCollection[0];
+            }
+            finally
+            {
+                store.Close();
+            }
+        }
     }
 }
