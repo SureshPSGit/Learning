6fd6b79c9527e74a54a0be7ca90e023261acc712
Nick Chapsas <elfocrash@gmail.com>
2020-03-22 22:01:06 +0000

    Removed the order consumer

--- AzureTutorials/ServiceBusExample/ServiceBus.Consumer/Startup.cs
+++ AzureTutorials/ServiceBusExample/ServiceBus.Consumer/Startup.cs
@@ -26,7 +26,6 @@ namespace ServiceBus.Consumer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddHostedService<CustomerConsumerService>();
-            services.AddHostedService<OrderConsumerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
