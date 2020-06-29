597df647d5a394ba5ec89ab5c286406af535d7e1
Nick Chapsas <elfocrash@gmail.com>
2020-06-29 18:42:48 +0100

    Topics settled

---
+++ GeneralTutorials/TipsAndTricks/src/AsyncActionsAreNotAwaited/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncActionsAreNotAwaited
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
