2617cb1f741bec599d26519aa02a29ac0a11dde6
Nick Chapsas <elfocrash@gmail.com>
2020-08-24 21:26:46 +0100

    Elastic search stuff yey

--- ASPNETCoreTutorials/ElasticSearchLogs/Controllers/WeatherForecastController.cs
+++ ASPNETCoreTutorials/ElasticSearchLogs/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace ElasticSearchLogs.Controllers
 {
@@ -16,24 +16,39 @@ namespace ElasticSearchLogs.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger logger)
         {
             _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            try
+            {
+                var rng = new Random();
+
+                if (rng.Next(0, 5) < 2)
                 {
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
-                })
-                .ToArray();
+                    throw new Exception("Oops what happened?");
+                }
+
+                return Ok(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                    {
+                        Date = DateTime.Now.AddDays(index),
+                        TemperatureC = rng.Next(-20, 55),
+                        Summary = Summaries[rng.Next(Summaries.Length)]
+                    })
+                    .ToArray());
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Something bad happened {CustomProperty}", 50);
+                return new StatusCodeResult(500);
+            }
+
         }
     }
 }
